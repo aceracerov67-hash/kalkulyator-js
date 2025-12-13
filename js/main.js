6f7070fdf4f3c5d1ce3e8cf83fe26a9431eb5cc6
farzona <aceracerov67@gmail.com>
2025-12-13 16:36:26 +0500

    change one

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const display = document.getElementById("display");
+
+function append(val) {
+  display.value += val;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function del() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calc() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = "Error";
+  }
+}
